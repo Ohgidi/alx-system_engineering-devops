af50600b149fe6e2afd58579a4cdbb3bbb294e35
Ohgidi <ohgidi@yahoo.com>
2023-01-14 16:05:30 -0800

    my second commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file in /tmp
+# create a file in /tmp with specific requirements
 
 file { 'school':
-  content => 'I love Puppet',
-  group   => 'www-data',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/school',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
